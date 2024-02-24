e283b85ad049ce2aee580b9b7427f10de7d747f6
gato365 <immanuelwilliams@gato365.com>
2024-02-24 04:44:53 -0800

    Easy commit

---
+++ old/learning_beautifulsoup-video 3.py
@@ -0,0 +1,27 @@
+import requests
+from bs4 import BeautifulSoup
+
+## Define URL
+url = input("What website do you want to scrape? ")
+
+## How many Lines
+num_lines = int(input("The number of lines please"))
+
+
+response = requests.get(url)
+
+print(response.status_code)
+
+
+
+htmlText = response.text
+
+soup = BeautifulSoup(htmlText, 'html.parser')
+
+paragraphs = soup.find_all('p')
+
+# print(paragraphs)
+
+for line in paragraphs[:num_lines]:
+    print(line.text.strip())    
+
